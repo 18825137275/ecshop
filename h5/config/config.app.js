3f814dafb2bb0716ad67abddaac1f40591e2e7ce
cheny@mlxing.com <cheny@mlxing.com>
2017-04-10 09:33:58 +0800

    first commit

---
+++ h5/config/config.app.js
@@ -0,0 +1,11 @@
+// https://api.ecshop36.geek-zoo.net
+// https://ecshoptest.xyunqi.com
+var GLOBAL_CONFIG = {
+	'API_HOST': 'http://api.ecshop3.6.geek-zoo.net',
+	'APP_NAME': 'Demo',
+	'APP_DESC': 'This is a demo',
+	'APP_KEYWORDS': 'Demo, Shop',
+	'DEBUG': false,
+	'ENCRYPTED': false,
+	'FOR_WEIXIN': false
+};
